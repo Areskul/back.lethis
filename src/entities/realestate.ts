1faecbcdd528a633e5a6ff29dedf0508842f5326
Areskul <the.only.areskul@gmail.com>
2021-06-18 15:10:45 +0200

    good field names

--- src/entities/realestate.ts
+++ src/entities/realestate.ts
@@ -14,55 +14,55 @@ registerEnumType(Fiscality, {
 
 @ObjectType()
 @Entity({
-  name: "RealEstate",
+  name: "realestate",
 })
 export class RealEstate extends BaseEntity {
   @Field(() => String, { nullable: true })
-  @Column({ name: "benefits", nullable: true })
+  @Column({ name: "fiscality", nullable: true })
   fiscality: string;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "wage", nullable: true })
+  @Column({ name: "owner", nullable: true })
   owner: string;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "landed", nullable: true })
+  @Column({ name: "name", nullable: true })
   name: string;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "others", nullable: true })
+  @Column({ name: "value", nullable: true })
   value: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "joint", nullable: true })
+  @Column({ name: "monthly", nullable: true })
   monthly: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "total", nullable: true })
+  @Column({ name: "qp", nullable: true })
   qp: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "qp", nullable: true })
+  @Column({ name: "result", nullable: true })
   result: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "result", nullable: true })
+  @Column({ name: "taxe", nullable: true })
   taxe: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "qp", nullable: true })
+  @Column({ name: "taxeResult", nullable: true })
   taxeResult: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "result", nullable: true })
+  @Column({ name: "rest", nullable: true })
   rest: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "result", nullable: true })
+  @Column({ name: "rate", nullable: true })
   rate: number;
 
   @Field(() => String, { nullable: true })
-  @Column({ name: "wage", nullable: true })
+  @Column({ name: "date", nullable: true })
   date: string;
 
   @Field(() => Client, { nullable: true })
